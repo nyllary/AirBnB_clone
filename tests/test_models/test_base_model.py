5c665d49855b6b384012f7140a9bc66f93e7e16a
JuggernautTej <tiwatej@gmail.com>
2024-03-12 13:00:44 -0700

    added tests but not working

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This test for the cases the BaseModel class"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """This houses all the tests for the BaseModel"""
+    def test_ifnotempty(self):
+        """Tests if an argument is passed"""
+        with self.assertRaises(TypeError):
+            test_instance = BaseModel(4)
+            print(base)
+
+    def test_forInstance(self):
+        """"Tests if the BaseModel is an instance"""
+        test_instance = BaseModel()
+        self.assertIsInstance(test_instance, BaseModel)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    
